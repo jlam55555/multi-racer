4498324d45f4f7e5de1be5c0086a0611834be832
Jonathan Lam <jlam55555@gmail.com>
2017-12-09 18:23:46 -0500

    fixed syntax error

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@ $(() => {
   // device orientation test
   // doesn't seem to work with jQuery
   window.addEventListener("deviceorientation", function(event) {
-    $("h1").text(JSON.stringify(event) + " " Math.floor(event.alpha) + " " + Math.floor(event.beta) + " " + Math.floor(event.gamma));
+    $("h1").text(JSON.stringify(event) + " " + Math.floor(event.alpha) + " " + Math.floor(event.beta) + " " + Math.floor(event.gamma));
   });
 
 });
