a02024be474a2ce278661c0ab2787d7fabef6efe
Jonathan Lam <jlam55555@gmail.com>
2017-12-09 18:33:11 -0500

    interpreted movement as turn/pedal

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,17 @@ $(() => {
   // store elements to avoid re-querying
   var $window = $(window);
 
+  // game turning
+  var turn;    // positive indicates turn right, negative indicates turn left
+  var pedal;   // positive indicates go forward, negative indicates go backward
+
   // device orientation test
   // doesn't seem to work with jQuery
   window.addEventListener("deviceorientation", function(event) {
-    $("h1").text(JSON.stringify(event) + " " + Math.floor(event.alpha) + " " + Math.floor(event.beta) + " " + Math.floor(event.gamma));
+    var turn = event.beta; // turning the device sideways
+    var pedal = event.gamma - 90; // tilting the device forwards/backwards
+                                  // straight up is no movement
+    $("h1").text(turn + " " + pedal);
   });
 
 });
