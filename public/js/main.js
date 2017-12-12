361fa0549db5ab93165008cb9bb510f17408f11e
Jonathan Lam <jlam55555@gmail.com>
2017-12-11 20:45:18 -0500

    created device link

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,10 @@ $(() => {
   // store elements to avoid re-querying
   var $window = $(window);
   var $body = $(document.body);
+  var $generateCode = $("#generateCode");
+  var $generatedCode = $("#generatedCode");
+  var $verifiedCode = $("#verifiedCode");
+  var $verifyCode = $("#verifyCode");
 
   // game turning
   var turn;    // positive indicates turn right, negative indicates turn left
@@ -15,8 +19,6 @@ $(() => {
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   var target = new THREE.Object3D();
-  //camera.rotation.x = -Math.PI/2;
-  //camera.position.y = 50;
   camera.rotation.y = Math.PI/2;
   camera.position.y = 5;
   camera.position.x = 15;
@@ -26,74 +28,82 @@ $(() => {
   $body.prepend(renderer.domElement);
 
   var spotLight = new THREE.SpotLight(0xffffff);
-  spotLight.position.set(0, 50, 0);
+  spotLight.position.set(0, 500, 0);
   scene.add(spotLight);
 
   var ambientLight = new THREE.AmbientLight(0x444444);
   scene.add(ambientLight);
 
-  var carShape = new THREE.Shape();
-  carShape.moveTo(1, 0);
-  carShape.lineTo(2, 0);
-  carShape.lineTo(2, 2);
-  carShape.lineTo(0, 2);
-  carShape.lineTo(-1, 3);
-  carShape.lineTo(-4, 3);
-  carShape.lineTo(-5, 2);
-  carShape.lineTo(-7, 2);
-  carShape.lineTo(-7, 0);
-  carShape.lineTo(-6, 0);
-  carShape.arc(1, 0, 1, Math.PI, 0, true);
-  carShape.lineTo(-1, 0);
-  carShape.arc(1, 0, 1, Math.PI, 0, true);
-  var carGeometry = new THREE.ExtrudeGeometry(carShape, {
-    steps: 10,
-    amount: 5,
-    curveSegments: 10,
-    bevelEnabled: false
-  });
-  var carMaterial = new THREE.MeshLambertMaterial({color: 0x0000ff});
-  var carMesh = new THREE.Mesh(carGeometry, carMaterial);
-  carMesh.add(camera);
-  scene.add(carMesh);
-  var underCarGeometry = new THREE.BoxGeometry(7, 1, 3);
-  var underCarMesh = new THREE.Mesh(underCarGeometry, carMaterial);
-  underCarMesh.position.y = 0.5;
-  underCarMesh.position.z = 2.5;
-  underCarMesh.position.x = -2.5;
-  carMesh.add(underCarMesh);
+  var Car = function() {
+    this.carShape = new THREE.Shape();
+    this.carShape.moveTo(1, 0);
+    this.carShape.lineTo(2, 0);
+    this.carShape.lineTo(2, 2);
+    this.carShape.lineTo(0, 2);
+    this.carShape.lineTo(-1, 3);
+    this.carShape.lineTo(-4, 3);
+    this.carShape.lineTo(-5, 2);
+    this.carShape.lineTo(-7, 2);
+    this.carShape.lineTo(-7, 0);
+    this.carShape.lineTo(-6, 0);
+    this.carShape.arc(1, 0, 1, Math.PI, 0, true);
+    this.carShape.lineTo(-1, 0);
+    this.carShape.arc(1, 0, 1, Math.PI, 0, true);
+    this.carGeometry = new THREE.ExtrudeGeometry(this.carShape, {
+      steps: 10,
+      amount: 5,
+      curveSegments: 10,
+      bevelEnabled: false
+    });
+    this.carMaterial = new THREE.MeshLambertMaterial({color: 0x0000ff});
+    this.carMesh = new THREE.Mesh(this.carGeometry, this.carMaterial);
+    this.underCarGeometry = new THREE.BoxGeometry(7, 1, 3);
+    this.underCarMesh = new THREE.Mesh(this.underCarGeometry, this.carMaterial);
+    this.underCarMesh.position.y = 0.5;
+    this.underCarMesh.position.z = 2.5;
+    this.underCarMesh.position.x = -2.5;
+    this.carMesh.add(this.underCarMesh);
+    this.wheelShape = new THREE.Shape();
+    this.wheelShape.arc(0, 0, 0.8, 0, Math.PI*2);
+    this.wheelGeometry = new THREE.ExtrudeGeometry(this.wheelShape, {
+      steps: 10,
+      amount: 0.75,
+      curveSegments: 10,
+      bevelEnabled: false
+    });
+    this.wheelMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
+    this.wheelMesh = new THREE.Mesh(this.wheelGeometry, this.wheelMaterial);
+    this.wheels = [];
+    var wheelsCoordinates = [[0, 0], [0, 4.25], [-5, 0], [-5, 4.25]];
+    for(var i = 0; i < 4; i++) {
+      var newWheel = this.wheelMesh.clone();
+      newWheel.position.x = wheelsCoordinates[i][0];
+      newWheel.position.z = wheelsCoordinates[i][1];
+      this.wheels.push(newWheel);
+      this.carMesh.add(newWheel);
+    }
+    this.pivot = new THREE.Object3D();
+    this.pivot.add(this.carMesh);
+    this.carMesh.applyMatrix(new THREE.Matrix4().makeTranslation(2.5, 0, -2.5));
+  };
+  var car = new Car();
+  scene.add(car.pivot);
+  car.carMesh.add(camera);
 
-  var wheelShape = new THREE.Shape();
-  wheelShape.arc(0, 0, 0.8, 0, Math.PI*2);
-  var wheelGeometry = new THREE.ExtrudeGeometry(wheelShape, {
-    steps: 10,
-    amount: 0.75,
-    curveSegments: 10,
-    bevelEnabled: false
-  });
-  var wheelMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
-  var wheelMesh = new THREE.Mesh(wheelGeometry, wheelMaterial);
-  var wheels = [];
-  var wheelsCoordinates = [[0, 0], [0, 4.25], [-5, 0], [-5, 4.25]];
-  for(var i = 0; i < 4; i++) {
-    var newWheel = wheelMesh.clone();
-    newWheel.position.x = wheelsCoordinates[i][0];
-    newWheel.position.z = wheelsCoordinates[i][1];
-    wheels.push(newWheel);
-    carMesh.add(newWheel);
-  }
-  var pivot = new THREE.Object3D();
-  pivot.add(carMesh);
-  carMesh.applyMatrix(new THREE.Matrix4().makeTranslation(2.5, 0, -2.5));
-  scene.add(pivot);
+  var otherCars = [];
 
   function animate() {
     requestAnimationFrame(animate);
-    pivot.rotation.y -= 0.0001 * (turn === undefined ? 10 : turn) * (pedal === undefined ? 10 : pedal);
-    for(wheel of wheels) {
-      wheel.rotation.z += 0.01 * (pedal === undefined ? 10 : pedal);
-      pivot.translateX(-0.005 * (pedal === undefined ? 10 : pedal));
+    // set turn, pedal if missing
+    if(true) {
+      turn = 10;
+      pedal = 10;
     }
+    car.pivot.rotation.y -= 0.0001 * turn * pedal;
+    for(wheel of car.wheels) {
+      wheel.rotation.z += 0.01 * pedal;
+    }
+    car.pivot.translateX(-0.02 * pedal);
     renderer.render(scene, camera);
   }
   animate();
@@ -103,15 +113,18 @@ $(() => {
   window.addEventListener("deviceorientation", function(event) {
     turn = Math.floor((event.gamma > 0 ? -1 : 1) * event.beta); // turning the device sideways
     pedal = Math.floor(45 - Math.abs(event.gamma));             // tilting the device forwards/backwards
-                                                                    // straight up is no movement
-    $("h1").text(Math.floor(turn) + " " + Math.floor(pedal));
+                                                                // 45 degree tilt is no movement
+                                                                // TODO: allow tilt calibration to user
+                                                                // TODO: allow portrait mode
+    //$("h1").text(Math.floor(turn) + " " + Math.floor(pedal));
   });
 
+  // get terrain map and implement
   socket.emit("getMap", function(mapData) {
     var elevations = mapData[0].map(e => e.r);
-    var planeGeometry = new THREE.PlaneGeometry(50, 50, 9, 9);
+    var planeGeometry = new THREE.PlaneGeometry(500, 500, 9, 9);
     for(var i = 0; i < elevations.length; i++) {
-      planeGeometry.vertices[i].z = elevations[i] / 255 * 5;
+      planeGeometry.vertices[i].z = elevations[i] / 255 * 100;
     }
     var planeMaterial = new THREE.MeshLambertMaterial({
       color: 0xffffff,
@@ -119,8 +132,26 @@ $(() => {
     });
     var planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
     planeMesh.rotation.x = -Math.PI/2;
-    planeMesh.position.y = -4;
+    planeMesh.position.y = -30;
     scene.add(planeMesh);
+  });
+
+  // generate sync code
+  $generateCode.click(() => {
+    socket.emit("generateCode", (generatedCode) => {
+      $generatedCode.text(generatedCode);
+      socket.on("codeVerified", () => {
+        $("h1").text("host " + generatedCode);
+      })
+    });
+  });
+
+  // verify sync code
+  $verifyCode.click(() => {
+    socket.emit("verifyCode", $verifiedCode.val(), (err) => {
+      if(err) console.log(err);
+      else $("h1").text("client " + $verifiedCode.val());
+    });
   });
 
 });
--- server.js
+++ server.js
@@ -29,11 +29,20 @@ var pixelGetter = require("pixel-getter");
 ███████  ██████   ██████ ██   ██ ███████    ██ ██ ██  ██████
 ******************************************************************************/
 var io = require("socket.io")(http);
+var sockets = [];
 io.on("connection", function(socket) {
   console.log("a user has connected");
+  sockets.push(socket);
+  var socketId = sockets.length-1;
+
+  // disconnect
+  socket.on("disconnect", () => {
+    sockets.splice(socketId, 1);
+    // TODO: delete paired socket reference if necessary
+  });
 
   // return map pixel data
-  socket.on("getMap", function(fn) {
+  socket.on("getMap", fn => {
     pixelGetter.get("./maps/map.png", function(error, pixels) {
       if(error) {
         console.log("Error retrieving map image data: " + error);
@@ -41,5 +50,45 @@ io.on("connection", function(socket) {
       }
       fn(pixels);
     });
-  })
+  });
+
+  // generate and return code
+  socket.on("generateCode", fn => {
+    for(var i = 0, code = ""; i < 5; i++) {
+      code += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+    // TODO: make sure codes are unique
+    // TODO: make sure user is not already assigned a code, or disconnect them and all connected codes
+    socket.code = code;
+    socket.host = true;
+    fn(code);
+  });
+
+  // verify code
+  socket.on("verifyCode", (code, fn) => {
+
+    // only match sockets that are hosts and that have a matching code
+    var matchingCodeHosts = sockets.filter(s => s.code === code && s.host);
+    // if code nonexistant return error
+    if(matchingCodeHosts.length === 0) {
+      return fn("code doesn't exist");
+    }
+
+    // if something is already connected return error
+    if(sockets.filter(s => s.code === code && !s.host).length > 0) {
+      return fn("host already matched");
+    }
+    // if host is self return error
+    if(matchingCodeHosts[0].id === socket.id) {
+      return fn("host cannot be client");
+    }
+    // success! set code, host, and socketPair (for both)
+    var host = matchingCodeHosts[0];
+    socket.code = code;
+    socket.host = false;
+    socket.socketPair = host;
+    host.socketPair = socket;
+    host.emit("codeVerified");
+    fn();
+  });
 });
